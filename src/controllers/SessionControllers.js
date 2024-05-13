f2dddb9847c6e817a11b42e9689d32af3eeb545f
1liviap <livia.fariapires74@gmail.com>
2024-05-13 08:46:09 -0300

    atualizando o projeto

---
+++ src/controllers/SessionControllers.js
@@ -0,0 +1,16 @@
+/*
+    index: listagem de sossoes
+    store (add): criar uma sesao
+    show (list): quando queremos listar um UNICA sessao
+    upddate: quando queremos alterar aluma sessao
+    destroy (delete): quando queremos deletar uma sessao
+*/
+
+import User from '../models/User'
+import * as Yup from 'yup'
+
+class SessionController{
+    async store(req, res) {
+        
+    }
+}
---
+++ src/models/User.js
@@ -0,0 +1,7 @@
+import { Schhema, model } from 'mongoose'
+
+const UserSchema = new Shema ({
+    email: String
+})
+
+export default model ('User', UserSchema)
